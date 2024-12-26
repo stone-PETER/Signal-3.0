59a314cfcad3d3657708967c4b330625caa41202
Alan Antony <voa.alan@gmail.com>
2024-12-26 10:20:36 +0530

    src

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+const App = () => {
+  return (
+    <div className="text-red-800 text-4xl">App</div>
+  )
+}
+export default App
---
+++ src/pages/Hero/Hero.jsx
@@ -0,0 +1,11 @@
+import RegBtn from "../../components/RegBtn"
+
+const Hero = () => {
+  return (
+    <div>
+        Hero
+        <RegBtn/>
+    </div>
+  )
+}
+export default Hero
