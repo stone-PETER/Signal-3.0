78568e70e9f1ccf2d9fab2bc010a2de57c2a19f6
Robin Samuelkutty <114683113+robinsamuelkutty@users.noreply.github.com>
2024-12-27 14:34:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Navbar from "./pages/Navbar/Navbar";
 import {Registration} from "./pages/Registration/registration"
 import Contact from "./pages/ContactUs/ContactUs";
 import Footer from "./pages/Footer/Footer";
+import Speaker from './pages/Speakers/Speakers'
 const App = () => {
   return (
     <>
       <Navbar />
       <Hero />
       <Registration/>
+      <Speaker/>
       <Contact />
       {/* <Footer/> */}
     </>
