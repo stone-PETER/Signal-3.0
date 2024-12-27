94da10d82f7cdd2059266639c12acd343c438d4b
Devakrishna07 <devakrishna1418@gmail.com>
2024-12-27 18:32:19 +0530

    about us page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
+import React from "react";
+import './index.css';
+import AboutUs from "./pages/About/AboutUs";
+
 const App = () => {
   return (
-    <div className="text-red-800 text-4xl">App</div>
-  )
+    <div>
+      <div className="text-red-800 text-4xl">App</div>
+      <AboutUs />  {/* Add the AboutUs component here */}
+    </div>
+  );
 }
-export default App
+
+export default App;
