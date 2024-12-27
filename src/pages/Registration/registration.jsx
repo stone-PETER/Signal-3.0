ccdb664028774bcade1c66c0e1b57854b7abf941
Abhinav K Manoj <abhinavmanoj120@gmail.com>
2024-12-27 21:12:45 +0530

    registration updates

--- src/pages/Registration/registration.jsx
+++ src/pages/Registration/registration.jsx
@@ -5,22 +5,22 @@ export const Registration=(props)=>
     return(
     <div id="registration">
         <div className="col-md-10 col-md-offset-1 section-title text-center">
-          <h2 className="title">Registration</h2>
+          <h2 className="title">Early Bird Registration</h2>
         </div>
          <div className="cards">
             <div className="card">
-                <h3>IEEE  Members</h3>
-                <h2>₹000</h2>
+                <h3>IEEE SPS Members</h3>
+                <h2>₹850</h2>
                 <a href="#" target="_blank">Register now</a>
             </div>
             <div className="card">
-                <h3>IEEE SPS Members</h3>
-                <h2>₹000</h2>
+                <h3>IEEE Members</h3>
+                <h2>₹900</h2>
                 <a href="#" target="_blank">Register now</a>
             </div>
             <div className="card">
                 <h3>Non-IEEE Members</h3>
-                <h2>₹000</h2>
+                <h2>₹1000</h2>
                 <a href="#" target="_blank" >Register now</a>
             </div>
         </div>
