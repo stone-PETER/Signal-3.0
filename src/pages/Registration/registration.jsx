71037bb53f3b0d66dc99571748921a8dbad9a5b9
Robin Samuelkutty <114683113+robinsamuelkutty@users.noreply.github.com>
2025-01-09 20:02:34 +0530

    Update registration.jsx

--- src/pages/Registration/registration.jsx
+++ src/pages/Registration/registration.jsx
@@ -34,40 +34,6 @@ export const Registration = () => {
       <Typography color="blue-gray" className="text-center font-normal">
         registrations are not refundable
       </Typography>
-      <div className="col-md-10 col-md-offset-1 section-title text-center">
-        <h2 className="title section-heading">
-          Early Bird Registrations Closed
-        </h2>
-      </div>
-      <div className="cards">
-        <div className="card relative">
-          <img
-            className="absolute pb-3 z-2 max-w-full max-h-full"
-            src="closed-stamp-3.png"
-            alt="Registration Closed"
-          />
-          <h3>IEEE SPS Members</h3>
-          <h2>₹850</h2>
-        </div>
-        <div className="card relative">
-          <img
-            className="absolute pb-3 z-2 max-w-full max-h-full"
-            src="closed-stamp-3.png"
-            alt="Registration Closed"
-          />
-          <h3>IEEE Members</h3>
-          <h2>₹950</h2>
-        </div>
-        <div className="card relative">
-          <img
-            className="absolute pb-3 z-2 max-w-full max-h-full"
-            src="closed-stamp-3.png"
-            alt="Registration Closed"
-          />
-          <h3>Non-IEEE Members</h3>
-          <h2>₹1050</h2>
-        </div>
-      </div>
     </div>
   );
 };
